0189f8f34e2e5427f1c56d2317ca19c68b1c211a
Alexey Panashchenko <alpa@ciklum.com>
2015-03-11 11:31:11 +0200

    Fix white page on anarcho logo click; Align app builds table values;Fix IT tests

--- anarchoApp/api_tests/test_team.py
+++ anarchoApp/api_tests/test_team.py
@@ -33,6 +33,11 @@ class TeamTest(AnarchoTestCase):
         self.assertIsNotNone(user_app, msg='UserApp for {0} not found'.format('test2@mail.com'))
         self.assertTrue(user_app.permission == 'w', msg='Wrong permission after update')
 
+    def test_can_not_found_app(self):
+        r = self.update_permission()
+        self.assert_status_code(r, 404)
+        self.assert_error_message(r, 'app_not_found')
+
     def test_permissions_remove(self):
         r = self.remove_permission(email=test_team_user_email, app_key=self.app_key)
         self.assert_status_code(r)
@@ -48,8 +53,9 @@ class TeamTest(AnarchoTestCase):
         self.assert_status_code(r, 403)
 
     def test_user_can_not_add_to_app_existing_user(self):
-        r = self.register(email=test_team_user_email, name='test_name2')
+        r = self.add_to_team(email=test_team_user_email, app_key=self.app_key, permission='r')
         self.assert_status_code(r, 409)
+        self.assert_error_message(r, 'user_with_current_email_already_exist')
 
     def test_email_format_validation(self):
         r = self.add_to_team(email='test3mail.com', app_key=self.app_key, permission='r')
